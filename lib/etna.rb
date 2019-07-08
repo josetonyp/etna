6dd139191e7c55735185ab50180be5a8d48370a0
Jose Antonio Pio Gil <jose.pio@solarisbank.de>
2019-07-08 12:51:48 +0200

    Explicitly require Forwardable on etna client.
    Ass discovered on Issue 3 there is missing require of
    forwardable ruby library during this gem initialization.

--- lib/etna.rb
+++ lib/etna.rb
@@ -2,6 +2,7 @@ require "etna/version"
 require "typhoeus"
 require 'json'
 require 'securerandom'
+require 'forwardable'
 
 ROOT_PATH = File.dirname(__FILE__)
 Dir.glob(ROOT_PATH + '/etna/concerns/*.rb') { |file| require file }
